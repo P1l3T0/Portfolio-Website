de1389f9f26aa19c97deba1963ca90d5419ca6ea
P1l3T0 <vgan3v@gmail.com>
2023-03-15 19:49:23 +0200

    abot me page functionalities done

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-function changeTabs() {
     var titleLinks = document.getElementsByClassName("title-link");
     var titleContents = document.getElementsByClassName("title-content");
-
-    function openTab() {
-        for (titleLink of titleLinks) {
-            titleLink.classlist.remove("active-link");
-        }
-
-        for(titleContent of titleContents) {
-            titleContent.classlist.remove("active-content");
-        }
         
-    }
-}
+    function openTab(tabname) {
+        for (titleLink of titleLinks) 
+            titleLink.classList.remove("active-link");
+
+        for(titleContent of titleContents) 
+            titleContent.classList.remove("active-content");
+        
+        event.currentTarget.classList.add("active-link")
+        document.getElementById(tabname).classList.add("active-content");
+    }    
