5536625804ada46de6d84b72b267834409878a5b
P1l3T0 <vgan3v@gmail.com>
2023-03-16 23:15:59 +0200

    almost done with mobile view, need to fix 1 div

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-    var titleLinks = document.getElementsByClassName("title-link");
-    var titleContents = document.getElementsByClassName("title-content");
-        
-    function openTab(tabname) {
-        for (titleLink of titleLinks) 
-            titleLink.classList.remove("active-link");
+var titleLinks = document.getElementsByClassName("title-link");
+var titleContents = document.getElementsByClassName("title-content");
+    
+function openTab(tabname) {
+    for (titleLink of titleLinks) 
+        titleLink.classList.remove("active-link");
 
-        for(titleContent of titleContents) 
-            titleContent.classList.remove("active-content");
-        
-        event.currentTarget.classList.add("active-link")
-        document.getElementById(tabname).classList.add("active-content");
-    }    
+    for(titleContent of titleContents) 
+        titleContent.classList.remove("active-content");
+    
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-content");
+}    
+
+var sideMenu = document.getElementById("side-menu");
+
+function openMenu() {
+    sideMenu.style.right = "0";
+    sideMenu.style.transition = "500ms ease";
+}
+
+function closeMenu() {
+    sideMenu.style.right = "-210px";
+    sideMenu.style.transition = "500ms ease";
+}
