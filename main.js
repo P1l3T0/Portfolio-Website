a3d7eddee12bc3493d5eaca90b34456e4532d477
P1l3T0 <vgan3v@gmail.com>
2023-03-17 17:34:47 +0200

    added lazy loading to the images

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+lazyLaoding();
 
 {
     var titleLinks = document.getElementsByClassName("title-link");
@@ -46,5 +46,24 @@
     }
 
     window.addEventListener('resize', changeImageSrc);
+}
 
+function lazyLaoding() {
+    const targets = [...document.querySelectorAll('img')];
+
+    const lazyLoad = target => {
+        const io = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const img = entry.target;
+                    const src = img.getAttribute('data-lazy');
+
+                    img.setAttribute('src', src);
+                    observer.disconnect();
+                };
+            });
+        });
+        io.observe(target);
+    };
+    targets.forEach(lazyLoad);
 }
