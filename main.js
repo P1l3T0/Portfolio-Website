c9703c70392d53cc722367fb20d24a281303f130
P1l3T0 <vgan3v@gmail.com>
2023-03-17 13:51:12 +0200

    mobile friendly

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
-var titleLinks = document.getElementsByClassName("title-link");
-var titleContents = document.getElementsByClassName("title-content");
-    
-function openTab(tabname) {
-    for (titleLink of titleLinks) 
-        titleLink.classList.remove("active-link");
 
-    for(titleContent of titleContents) 
-        titleContent.classList.remove("active-content");
-    
-    event.currentTarget.classList.add("active-link")
-    document.getElementById(tabname).classList.add("active-content");
-}    
 
-var sideMenu = document.getElementById("side-menu");
+{
+    var titleLinks = document.getElementsByClassName("title-link");
+    var titleContents = document.getElementsByClassName("title-content");
 
-function openMenu() {
-    sideMenu.style.right = "0";
-    sideMenu.style.transition = "500ms ease";
+    function openTab(tabname) {
+        for (titleLink of titleLinks)
+            titleLink.classList.remove("active-link");
+
+        for (titleContent of titleContents)
+            titleContent.classList.remove("active-content");
+
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-content");
+    }
 }
 
-function closeMenu() {
-    sideMenu.style.right = "-210px";
-    sideMenu.style.transition = "500ms ease";
+{
+    var sideMenu = document.getElementById("side-menu");
+
+    function openMenu() {
+        sideMenu.style.right = "0";
+        sideMenu.style.transition = "500ms ease";
+    }
+
+    function closeMenu() {
+        sideMenu.style.right = "-210px";
+        sideMenu.style.transition = "500ms ease";
+    }
+}
+
+{
+    const chickenWebsite = document.getElementById('chicken-website');
+    const paint = document.getElementById('paint');
+
+    function changeImageSrc() {
+        if (window.innerWidth < 620) {
+            chickenWebsite.src = '../pictures/chicken-website-mobile.webp';
+            paint.src = '../pictures/paint-mobile.webp';
+        } else {
+            chickenWebsite.src = '../pictures/chicken-website.webp';
+            paint.src = '../pictures/paint.webp';
+
+        }
+    }
+
+    window.addEventListener('resize', changeImageSrc);
+
 }
