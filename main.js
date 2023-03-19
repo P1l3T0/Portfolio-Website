4610704adf2a45f9fda5938a580767ff6d342036
P1l3T0 <vgan3v@gmail.com>
2023-03-19 20:05:04 +0200

    added blur effect

--- main.js
+++ main.js
@@ -16,15 +16,26 @@
 
 {
     var sideMenu = document.getElementById("side-menu");
+    var blurEffect = document.getElementById("blur-overlay");
 
     function openMenu() {
         sideMenu.style.right = "0";
         sideMenu.style.transition = "500ms ease";
+
+        blurEffect.style.pointerEvents = 'auto';
+        setTimeout(() => {
+            blurEffect.style.display = 'block';
+        }, 1);
     }
 
     function closeMenu() {
         sideMenu.style.right = "-210px";
         sideMenu.style.transition = "500ms ease";
+
+        blurEffect.style.pointerEvents = 'none';
+        setTimeout(() => {
+            blurEffect.style.display = 'none';
+        }, 1);
     }
 }
 
@@ -34,11 +45,11 @@
 
     function changeImageSrc() {
         if (window.innerWidth < 1500) {
-            chickenWebsite.src = '../pictures/chicken-website-mobile.webp';
-            paint.src = '../pictures/paint-mobile.webp';
+            chickenWebsite.src = '/pictures/chicken-website-mobile.webp';
+            paint.src = '/pictures/paint-mobile.webp';
         } else {
-            chickenWebsite.src = '../pictures/chicken-website.webp';
-            paint.src = '../pictures/paint.webp';
+            chickenWebsite.src = '/pictures/chicken-website.webp';
+            paint.src = '/pictures/paint.webp';
 
         }
     }
