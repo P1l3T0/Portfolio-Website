84171e43aa6c940b3ac389d81e3c14eaac661f29
P1l3T0 <vgan3v@gmail.com>
2023-03-19 17:03:09 +0200

    projects section is now a flexbox

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-lazyLaoding();
-
 {
     var titleLinks = document.getElementsByClassName("title-link");
     var titleContents = document.getElementsByClassName("title-content");
@@ -46,24 +44,4 @@ lazyLaoding();
     }
 
     window.addEventListener('resize', changeImageSrc);
-}
-
-function lazyLaoding() {
-    const targets = [...document.querySelectorAll('img')];
-
-    const lazyLoad = target => {
-        const io = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    const img = entry.target;
-                    const src = img.getAttribute('data-lazy');
-
-                    img.setAttribute('src', src);
-                    observer.disconnect();
-                };
-            });
-        });
-        io.observe(target);
-    };
-    targets.forEach(lazyLoad);
 }
