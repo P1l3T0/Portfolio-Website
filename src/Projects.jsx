813a86fd7136add84162f504df13fac2486d7331
P1l3T0 <vgan3v@gmail.com>
2023-05-13 22:01:13 +0300

    updated cv and made projects into components

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,3 +1,4 @@
+import { data } from './ProjectsData';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
@@ -21,30 +22,17 @@ export function Projects() {
             <h1 className="sub-title"><span>My</span> Projects</h1>
             <div className="projects-container">
                 <div className="work-list">
-                    <div className="work">
-                        <img src="./assets/chicken-website.webp" id="chicken-website" alt="picture of my chicken website" />
-                        <div className="layer">
-                            <h1>Chicken Website</h1>
-                            <p>
-                                Website, dedicated to chicken, created using HTML, CSS, JavaScript
-                                and PHP for the server side
-                            </p>
-                            <a href="https://github.com/P1l3T0/Chicken-Website" target="_blank">
-                                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" /></a>
+                    {data.map(project =>
+                        <div className="work" key={project.id}>
+                            <img src={project.src} id={project.id} alt={project.alt} />
+                            <div className="layer">
+                                <h1>{project.name}</h1>
+                                <p>{project.description}</p>
+                                <a href={project.link} target="_blank">
+                                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" /></a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="work">
-                        <img src="./assets/paint.webp" id="paint" alt="picture of my shape editor application" />
-                        <div className="layer">
-                            <h1>2D Shape Editor</h1>
-                            <p>
-                                Application for creating 2D shapes and drawing with freehand,
-                                using vector graphics, created with C# and Windows Forms
-                            </p>
-                            <a href="https://github.com/P1l3T0/2D-Shape-Editor" target="_blank">
-                                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" /></a>
-                        </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </div >
---
+++ src/ProjectsData.jsx
@@ -0,0 +1,17 @@
+export const data = [{
+    id: 1,
+    name: "Chicken Website",
+    id: "chicken-website",
+    src: "./assets/chicken-website.webp",
+    alt: "picture of my chicken website",
+    description: "Website, dedicated to chicken, created using HTML, CSS, JavaScript and PHP for the server side",
+    link: "https://github.com/P1l3T0/Chicken-Website"
+}, {
+    id: 2,
+    name: "2D Shape Editor",
+    id: "paint",
+    src: "./assets/chicken-website.webp",
+    alt: "picture of my shape editor application",
+    description: "Application for creating 2D shapes and drawing with freehand, using vector graphics, created with C# and Windows Forms",
+    link: "https://github.com/P1l3T0/2D-Shape-Editor"
+}];
