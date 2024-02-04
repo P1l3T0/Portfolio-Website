f80ae0a15e04e154f3ca72fd85b3222881dc89fe
P1l3T0 <vgan3v@gmail.com>
2024-02-04 18:37:05 +0200

    feat(projects): add data for the newest project

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -3,39 +3,42 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const Projects = () => {
-    window.addEventListener("resize", function () {
-        const chickenWebsite = document.getElementById('chicken-website');
-        const paint = document.getElementById('paint');
+  window.addEventListener("resize", function () {
+    const paint = document.getElementById('paint');
+    const iFitTracker = document.getElementById('ifit-tracker');
+    const chickenWebsite = document.getElementById('chicken-website');
 
-        if (window.innerWidth < 1000) {
-            chickenWebsite.src = './assets/chicken-website-mobile.webp';
-            paint.src = './assets/paint-mobile.webp';
-        } else {
-            chickenWebsite.src = './assets/chicken-website.webp';
-            paint.src = './assets/paint.webp';
-        }
-    });
+    if (window.innerWidth < 1000) {
+      paint.src = './assets/paint-mobile.webp';
+      iFitTracker.src = './assets/ifittracker-mobile.webp';
+      chickenWebsite.src = './assets/chicken-website-mobile.webp';
+    } else {
+      paint.src = './assets/paint.webp';
+      iFitTracker.src = './assets/ifittracker.webp';
+      chickenWebsite.src = './assets/chicken-website.webp';
+    }
+  });
 
-    return (
-        <div id="projects" >
-            <h1 className="sub-title"><span>My</span> Projects</h1>
-            <div className="projects-container">
-                <div className="work-list">
-                    {data.map((project, index) => (
-                        <div className="work" key={index}>
-                            <img src={project.src} id={project.id} alt={project.alt} />
-                            <div className="layer">
-                                <h1>{project.name}</h1>
-                                <p>{project.description}</p>
-                                <a href={project.link} target="_blank">
-                                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" /></a>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <div id="projects" >
+      <h1 className="sub-title"><span>My</span> Projects</h1>
+      <div className="projects-container">
+        <div className="work-list">
+          {data.map((project, index) => (
+            <div className="work" key={index}>
+              <img src={project.src} id={project.id} alt={project.alt} />
+              <div className="layer">
+                <h1>{project.name}</h1>
+                <p>{project.description}</p>
+                <a href={project.link} target="_blank">
+                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" /></a>
+              </div>
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
